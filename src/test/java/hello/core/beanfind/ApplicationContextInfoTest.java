250b0e2949233562e4bec93cef006e0fe88212fb
yang <acs0705@naver.com>
2026-03-18 22:36:58 +0900

    yang

---
+++ src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -0,0 +1,11 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextInfoTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+}
