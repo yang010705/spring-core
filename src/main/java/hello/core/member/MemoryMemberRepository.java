9c0e623fa9bcd06bdc02afbf0372ae7c97758e02
yang010705 <acs0705@naver.com>
2026-03-25 14:56:30 +0900

    memrge 충돌 해결

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -1,8 +1,11 @@
 package hello.core.member;
 
+import org.springframework.context.annotation.ComponentScan;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@ComponentScan
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long, Member> store = new HashMap<>();
